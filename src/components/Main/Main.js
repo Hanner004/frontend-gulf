56687cfe7411826214ee91116f4927abe87e7c69
Hanner004 <hanerdelahoz900@hotmail.com>
2021-11-25 11:58:46 -0500

    navbar

---
+++ src/components/Main/Main.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div className="container-fluid main">
+        {/* <div className="container">
+          <div className="row">
+            <div className="col">Hola nombre user,</div>
+          </div>
+          <div className="row">
+            <div className="col">
+              Lorem ipsum dolor sit amet, consectetur adipiscing.
+            </div>
+          </div>
+        </div> */}
+      </div>
+    );
+  }
+}
--- src/components/props.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Msg = (props) => <p>{props.name}</p>;
-
-export default Msg;
