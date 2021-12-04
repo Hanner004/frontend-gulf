8742ff190645c95558ababec9d9dcf723689798f
Hanner <77119827+Hanner004@users.noreply.github.com>
2021-12-03 21:23:05 -0500

    import fonts

--- src/components/Auth/Login/Form.jsx
+++ src/components/Auth/Login/Form.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import "../styles.css";
+import "../Auth.css";
 
 export default class Form extends Component {
   render() {
