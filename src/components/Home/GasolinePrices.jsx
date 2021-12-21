0f9f5605b586a0933a6f1d8d47e3b3563031fd24
falquezm <90979848+falquezm@users.noreply.github.com>
2021-12-21 13:30:53 -0500

    Revert "Componente GAsolinePrice"

--- src/components/Home/GasolinePrices.jsx
+++ src/components/Home/GasolinePrices.jsx
@@ -2,41 +2,22 @@ import React, { Component } from "react";
 import list from "../../data/data.json"
 
 export default class GasolinePrices extends Component {
-  constructor(){
-    super();
-    this.state = {
-      listGasoline : {}
-    }
-  }
-
-  componentDidMount() {
-    const { session } = this.props;
-    fetch("http://localhost:4000/api/gasoline",{
-      method: 'GET',
-      headers: {'Authorization': 'Bearer '+ session.token},
-    })
-      .then((response) => response.json())
-      .then((response) => {this.setState({listGasoline : response.data})})
-      .catch((err)=>{console.log(err)});
-  }
-
-  render() { 
-    const datos = this.state.listGasoline
+  render() {
     return (
       <div className="row gasoline-user">
         <div className="card-user">
           <h5 className="card-header title-gas-user py-3">Gasolinas</h5>
           <div className="card-body">
             {
-              datos.length &&
-              datos.map((gas)=>{
+              list.gasolines.length &&
+              list.gasolines.map((gas)=>{
                 return(
                   <div key={gas._id} className="row item-gas-user">
                     <div className="col">
-                      {gas.type}<br /><p>{gas.status}</p> 
+                      {gas.tipe}<br /><p>{gas.state}</p> 
                     </div>
                     <div className="col">
-                      {gas.price.toLocaleString('es-co', { style: 'currency', currency: 'COP' })}<br /><p>($/gal)</p> 
+                      ${gas.price}<br /><p>($/gal)</p> 
                     </div>
                   </div>
                 )
